4ad7269022eee7f328ca0919063296321e9637b8
dhairyaLala <138852380+dhairyaLala@users.noreply.github.com>
2023-07-08 13:11:47 +0530

    Feature/post request (#12)
    * Post request

---
+++ src/main/java/com/automation/ui/apiUtils/ServiceUtils.java
@@ -0,0 +1,15 @@
+package com.automation.ui.apiUtils;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ServiceUtils {
+
+    public String sendPostRequest(String baseURL, String body) {
+        Response response = RestAssured.given().baseUri(baseURL).contentType(ContentType.JSON).body(body).post().then().extract().response();
+        return response.getBody().asString();
+    }
+}
