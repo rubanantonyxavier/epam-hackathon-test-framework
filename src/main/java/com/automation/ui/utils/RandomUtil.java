da178c30ca48aca0e4fa49525d928387d394e0c0
dhairyaLala <dhairyalala@gmail.com>
2023-07-08 11:34:26 +0530

    Update code for generate fibonacci

--- src/main/java/com/automation/ui/utils/RandomUtil.java
+++ src/main/java/com/automation/ui/utils/RandomUtil.java
@@ -4,6 +4,8 @@ import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
 
 @UtilityClass
 public class RandomUtil {
@@ -11,5 +13,24 @@ public class RandomUtil {
     @SneakyThrows
     public int getRandomNumber(int maxDigit) {
         return SecureRandom.getInstanceStrong().nextInt(maxDigit);
+    }
+
+    public List<Integer> getEvenNumbersFromFibonacci(int limit) {
+        List<Integer> evenFibonacciNumbers = new ArrayList<>();
+
+        int a = 0;
+        int b = 1;
+        int fibonacciNumber = a + b;
+
+        while (fibonacciNumber <= limit) {
+            if (fibonacciNumber % 2 == 0) {
+                evenFibonacciNumbers.add(fibonacciNumber);
+            }
+
+            a = b;
+            b = fibonacciNumber;
+            fibonacciNumber = a + b;
+        }
+        return evenFibonacciNumbers;
     }
 }
