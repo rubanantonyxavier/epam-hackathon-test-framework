59d9aa5ea26ed42e2749a3706bab78460e8a44a4
dhairyaLala <dhairyalala@gmail.com>
2023-07-08 11:27:57 +0530

    generate fibonacci

--- src/main/java/com/automation/ui/pages/ipl/FibonacciPage.java
+++ src/main/java/com/automation/ui/pages/ipl/FibonacciPage.java
@@ -2,18 +2,25 @@ package com.automation.ui.pages.ipl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class FibonacciPage {
 
-    public List<Integer> fibonacciEvenNumbers() {
-        int limit = 40;
-        return Stream.iterate(new int[]{0, 1}, fib -> new int[]{fib[1], fib[0] + fib[1]})
-                .mapToInt(fib -> fib[0])
-                .limit(limit)
-                .filter(num -> num % 2 == 0)
-                .boxed()
-                .collect(Collectors.toCollection(ArrayList::new));
+    public List<Integer> fibonacciEvenNumbers(int limit) {
+        List<Integer> evenFibonacciNumbers = new ArrayList<>();
+
+        int a = 0;
+        int b = 1;
+        int fibonacciNumber = a + b;
+
+        while (fibonacciNumber <= limit) {
+            if (fibonacciNumber % 2 == 0) {
+                evenFibonacciNumbers.add(fibonacciNumber);
+            }
+
+            a = b;
+            b = fibonacciNumber;
+            fibonacciNumber = a + b;
+        }
+        return evenFibonacciNumbers;
     }
 }
