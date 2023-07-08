e21631813aecddda8431a3464521b5e0969bd3b4
SMShubhamMittal <shubhammittal26yahoo@gmail.com>
2023-07-08 11:35:50 +0530

    Task1: StepDefs

---
+++ src/test/java/com/automation/ui/step_definitions/ipl/IplPageSteps.java
@@ -0,0 +1,16 @@
+package com.automation.ui.step_definitions.ipl;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class IplPageSteps {
+    
+    @Then("IPL page is opened")
+    public void iplPageIsOpened() {
+        
+    }
+
+    @When("Navigate to STATS in Header")
+    public void navigateToSTATSInHeader() {
+    }
+}
---
+++ src/test/java/com/automation/ui/step_definitions/ipl/StatsPageSteps.java
@@ -0,0 +1,33 @@
+package com.automation.ui.step_definitions.ipl;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StatsPageSteps {
+
+    @Then("STATS page is opened")
+    public void statsPageIsOpened() {
+
+    }
+
+    @When("User clicks {string} radio button in StatsType dropdown and select {string} option")
+    public void userClicksRadioButtonInStatsTypeDropdownAndSelectOption(String statsType, String capOption) {
+    }
+
+    @Then("{string} is displayed in StatsType dropdown")
+    public void isDisplayedInStatsTypeDropdown(String statsType) {
+    }
+
+    @When("User clicks {string} button")
+    public void userClicksButton(String buttonText) {
+    }
+
+    @And("User capture the Stats of Players")
+    public void userCaptureTheStatsOfPlayers() {
+    }
+
+    @And("Select a player based on the response received from API")
+    public void selectAPlayerBasedOnTheResponseReceivedFromAPI() {
+    }
+}
